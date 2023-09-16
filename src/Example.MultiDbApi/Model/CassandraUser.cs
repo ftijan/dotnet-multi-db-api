d563f5a03db8b492c38f8cc1c358eaaf67668737
Filip Tijan <ftijan@gmail.com>
2023-09-16 02:27:07 +0100

    add sample cassandra and redis logic

---
+++ src/Example.MultiDbApi/Model/CassandraUser.cs
@@ -0,0 +1,24 @@
+﻿using Cassandra.Mapping.Attributes;
+
+namespace Example.MultiDbApi.Model;
+
+// Assumes below keyspace and table have been already created:
+[Table("user", Keyspace = "test")]
+public class CassandraUser
+{
+	public CassandraUser(Guid id, string firstName, string lastName)
+	{
+		Id = id;
+		FirstName = firstName;
+		LastName = lastName;
+	}
+
+	[Column("id")]
+    public Guid Id { get; set; }		
+
+	[Column("firstname", Type = typeof(string))]
+	public string FirstName { get; set; }
+
+	[Column("lastname", Type = typeof(string))]
+	public string LastName { get; set; }
+}
--- src/Example.MultiDbApi/Program.cs
+++ src/Example.MultiDbApi/Program.cs
@@ -1,3 +1,6 @@
+using Cassandra;
+using StackExchange.Redis;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+Cluster cluster = Cluster
+				.Builder()
+				.WithCredentials("cassandra", "cassandra")
+				.WithPort(9042)
+				.AddContactPoint("localhost")
+				.Build();
+builder.Services.AddSingleton<ICluster>(cluster);
+
+
+var options = ConfigurationOptions.Parse("localhost:6379");
+options.User = "user";
+options.Password = "pass";
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(options);
+builder.Services.AddSingleton<IConnectionMultiplexer>(redis);
 
 var app = builder.Build();
 
